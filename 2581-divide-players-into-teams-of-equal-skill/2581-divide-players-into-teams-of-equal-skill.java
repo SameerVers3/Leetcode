a79aaa7f9f37eb1f40f0099718d064b5e3d1bbff
Sameer <111224603+SameerVers3@users.noreply.github.com>
2024-10-05 03:03:01 +0500

    Time: 17 ms (36.75%), Space: 57.2 MB (87.75%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -1,30 +1,38 @@
+import java.util.HashMap;
+
 class Solution {
     public long dividePlayers(int[] skill) {
-        int totalSum = Arrays.stream(skill).sum();
-        if (totalSum % (skill.length / 2) != 0) {
+        int sum = 0;
+        for (int s : skill) {
+            sum += s;
+        }
+        
+        int n = skill.length;
+        if (sum % (n / 2) != 0) {
             return -1;
         }
-
-
-        Arrays.sort(skill);
-
-        int i = 0;
-        int j = skill.length - 1;
-
-        int sum = skill[i] + skill[j];
-
+        
+        int tsum = sum / (n / 2);
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
         long res = 0L;
-
-        while (i < j) {
-            if (skill[i] + skill[j] != sum) {
-                return -1;
+        
+        for (int s : skill) {
+            int c = tsum - s;
+            
+            if (freqMap.getOrDefault(c, 0) > 0) {
+                res += (long) s * c;
+                freqMap.put(c, freqMap.get(c) - 1);
+            } else {
+                freqMap.put(s, freqMap.getOrDefault(s, 0) + 1);
             }
-
-            res += (long) (skill[i] * skill[j]);
-            i++;
-            j--;
         }
 
+        for (int c : freqMap.values()) {
+            if (c != 0) {
+                return -1;
+            }
+        }
+        
         return res;
     }
 }
